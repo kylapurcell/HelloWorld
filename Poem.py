80e9357aaaae0480a063d5cd9aa0ccb90d5b9b4c
kylapurcell <46577009+kylapurcell@users.noreply.github.com>
2019-01-15 13:45:34 -0800

    Initial commit

---
+++ Poem.py
@@ -0,0 +1,6 @@
+print("Roses are red")
+print("Violets are blue")
+print("Sugar is sweet")
+print("But I have commitment issues")
+print("So I'd rather just be friends")
+print("At this point in our relationship")
---
+++ Temperature.py
@@ -0,0 +1,3 @@
+degreescelsius= 10
+degreesfahrenheit= degreescelsius * 9/5 + 32
+print(str(degreescelsius) + " " "degrees celsius" + " " + "equals" + " " + str(degreesfahrenheit) + " " + "degrees Fahrenheit")
